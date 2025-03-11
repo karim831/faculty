45bb151033b333d0d96fadf5e58ac16f4591d19f
kareem <kareem@kareem.com>
2025-03-12 01:32:32 +0200

    ...

---
+++ labs/mips/qtspim_9.1.24/qtspim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/home/kareem/studying/faculty/labs/mips/qtspim_9.1.24/usr/lib/qtspim/lib
+
+export QT_QPA_PLATFORM_PLUGIN_PATH=/home/kareem/studying/faculty/labs/mips/qtspim_9.1.24/usr/lib/qtspim/plugins
+
+exec /home/kareem/studying/faculty/labs/mips/qtspim_9.1.24/usr/lib/qtspim/bin/qtspim
