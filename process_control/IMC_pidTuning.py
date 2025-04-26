b6f4603b760032e736b130b1adf73b92dba975b7
kareem <kareem@kareem.com>
2025-04-26 12:57:58 +0300

    ...

--- process_control/IMC_pidTuning.py
+++ process_control/IMC_pidTuning.py
@@ -27,32 +27,37 @@ def get_closed_loop(K, tau, theta, lam):
     return T, (Kc, Ti, Td)
 
 # Initial parameters
-K0, tau0, theta0, lam0 = 1.0, 1.0, 0.5, 1.0
+K0, tau0, theta0, lam0, input0 = 1.0, 1.0, 0.5, 1.0, 1.0
 
 fig, ax = plt.subplots()
-plt.subplots_adjust(left=0.25, bottom=0.4)
+plt.subplots_adjust(left=0.25, bottom=0.5)
 t = np.linspace(0, 20, 1000)
 
 system, (Kc0, Ti0, Td0) = get_closed_loop(K0, tau0, theta0, lam0)
 t_out, y_out = ctrl.step_response(system, t)
-line, = ax.plot(t_out, y_out)
+line, = ax.plot(t_out, y_out, label="System Output")
+input_line, = ax.plot(t_out, input0 * np.ones_like(t_out), 'r--', label="Step Input")  # red dashed line for input
 ax.set_title("Step Response with IMC Tuned PID")
 ax.set_xlabel("Time")
 ax.set_ylabel("Output")
+ax.legend()
+ax.set_ylim(-2, 2)
 
 # Sliders for parameters
 axcolor = 'lightgoldenrodyellow'
 slider_axes = {
-    'K': plt.axes([0.25, 0.30, 0.65, 0.03], facecolor=axcolor),
-    'tau': plt.axes([0.25, 0.25, 0.65, 0.03], facecolor=axcolor),
-    'theta': plt.axes([0.25, 0.20, 0.65, 0.03], facecolor=axcolor),
-    'lambda': plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+    'K': plt.axes([0.25, 0.40, 0.65, 0.03], facecolor=axcolor),
+    'tau': plt.axes([0.25, 0.35, 0.65, 0.03], facecolor=axcolor),
+    'theta': plt.axes([0.25, 0.30, 0.65, 0.03], facecolor=axcolor),
+    'lambda': plt.axes([0.25, 0.25, 0.65, 0.03], facecolor=axcolor),
+    'input': plt.axes([0.25, 0.20, 0.65, 0.03], facecolor=axcolor)
 }
 sliders = {
     'K': Slider(slider_axes['K'], 'K', 0.1, 10.0, valinit=K0),
     'tau': Slider(slider_axes['tau'], 'Tau', 0.1, 10.0, valinit=tau0),
     'theta': Slider(slider_axes['theta'], 'Theta', 0.0, 5.0, valinit=theta0),
-    'lambda': Slider(slider_axes['lambda'], 'Lambda', 0.1, 10.0, valinit=lam0)
+    'lambda': Slider(slider_axes['lambda'], 'Lambda', 0.1, 10.0, valinit=lam0),
+    'input': Slider(slider_axes['input'], 'Input', 0.1, 5.0, valinit=input0)
 }
 
 def update(val):
@@ -60,13 +65,18 @@ def update(val):
     tau = sliders['tau'].val
     theta = sliders['theta'].val
     lam = sliders['lambda'].val
+    input_amp = sliders['input'].val
 
     new_sys, (Kc, Ti, Td) = get_closed_loop(K, tau, theta, lam)
     t_out, y_out = ctrl.step_response(new_sys, t)
-    line.set_ydata(y_out)
+    line.set_ydata(y_out * input_amp)  # scale output according to input
+    input_line.set_ydata(input_amp * np.ones_like(t_out))  # adjust input line
     fig.canvas.draw_idle()
 
 for slider in sliders.values():
     slider.on_changed(update)
+
+# Enable zoom and pan interactivity in the plot
+plt.rcParams['toolbar'] = 'toolbar2'  # Add this line to enable zoom toolbar
 
 plt.show()
