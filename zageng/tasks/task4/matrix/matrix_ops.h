702ed0bc7858772788b4e4f6bbdcb002d3a74c2f
karim831 <112541594+karim831@users.noreply.github.com>
2025-10-06 19:54:28 +0300

    Delete zageng directory

--- zageng/tasks/task4/matrix/matrix_ops.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MATRIX_OPS_H__
-
-    void sum(int n, int m, int matrix1[n][m], int matrix2[n][m]);
-    void diff(int n, int m, int matrix1[n][m], int matrix2[n][m]);
-    void mul(int n, int m, int k, int matrix1[n][m], int matrix2[m][k]);
-
-#endif
