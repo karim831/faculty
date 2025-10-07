1d01eb8c5a92cb616a1bc00c404c91a103b556a6
kareem <kareem@kareem.com>
2025-10-07 21:14:12 +0300

    finish 1d localization

---
+++ Fourth/Labs/Localization/js/main.js
@@ -0,0 +1,5 @@
+import { Localization1D } from "./services/Localization1D.js";
+
+const world = ['High','Low','Low','High','Low','High','Low','Low'];
+
+window.localizer =  new Localization1D(world);
--- Fourth/Labs/Localization/js/services/Localization1D.js
+++ Fourth/Labs/Localization/js/services/Localization1D.js
@@ -1,36 +1,37 @@
-class Localization{
-    constructor(){
-        this.world = ['High','Low','Low','High','Low','High','Low','Low'];
-        
-        /**
-         * @property {number[]} bel
-         */
-        this.bel = [];
+export class Localization1D{
+    /**
+     * 
+     * @param {string[]} world 
+     */
+    constructor(world){
+        this.world = world;
+        this.believe = [];
 
         this.resetBelieve(); 
     }
 
     resetBelieve(){
-        this.bel = Array(this.world.length).fill(1 / this.world.length);
-        this.render();
+        this.believe = Array(this.world.length).fill(1 / this.world.length);
+        return this.render();
     }
-
+    
     /**
      * 
      * @param {string} measurement
      */
     sense(measurement, pHit = .7 , pMiss = .2){
-        const newBel = this.bel.map((b,i) => {
+        const newBel = this.believe.map((b,i) => {
            return b * (measurement.toLowerCase() == this.world[i].toLowerCase() ? pHit : pMiss);
         });
 
         const sum = newBel.reduce((prev, curr) => prev + curr);
         
-        this.bel = newBel.map(b => b / sum);
-
+        this.believe = newBel.map(b => b / sum);
+        
         this.render();
-        this.printbel();
+        this.printBelieve();
     }
+
 
     /**
      * @param {number} step
@@ -39,38 +40,24 @@ class Localization{
         const n = this.world.length;
         let newBel = Array(n).fill(0);
 
-        this.bel.forEach((b, i) => {
+        this.believe.forEach((b, i) => {
             newBel[this.#mod(i + step,n)] += b * pExact;
             newBel[this.#mod(i + step + 1,n)] += b * pOvershoot;
             newBel[this.#mod(i + step - 1,n)] += b * pUndershoot;
         });
         
-        this.bel = newBel;
+        this.believe = newBel;
         
         this.render();
-        this.printbel();
-    }
-
-
-
-    /**
-     * 
-     * @param {number} x 
-     * @param {number} m 
-     */
-    #mod(x, m){
-        const modelusResult = x % m;
-        if(modelusResult < 0)
-            return m + modelusResult;
-        return modelusResult;
+        this.printBelieve();
     }
 
     render(){
         const container = document.getElementById('world');
         container.innerHTML = '';
 
-        const maxBel = Math.max(...this.bel);
-        this.bel.forEach((b, i) => {
+        const maxBel = Math.max(...this.believe);
+        this.believe.forEach((b, i) => {
           const cell = document.createElement('div');
           cell.className = 'cell ' + this.world[i].toLowerCase();
 
@@ -90,15 +77,25 @@ class Localization{
         });
     }
 
-    printbel() {
+    printBelieve(){
         console.log("Current belief:");
-        this.bel.forEach((b, i) => {
+        this.believe.forEach((b, i) => {
             console.log(`Cell ${i} (${this.world[i]}): ${b.toFixed(4)}`);
         });
-        console.log("Sum:", this.bel.reduce((a, b) => a + b, 0).toFixed(4));
+        console.log("Sum:", this.believe.reduce((a, b) => a + b, 0).toFixed(4));
     }
 
 
-}
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} m 
+     */
+    #mod(x, m){
+        const modelusResult = x % m;
+        if(modelusResult < 0)
+            return m + modelusResult;
+        return modelusResult;
+    }
 
-let localizer = new Localization();
+}
